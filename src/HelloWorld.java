4a8391f85fac9056f4a3a109280ee310d8fc06c8
SGhosh <shrutikaghosh@gmail.com>
2018-07-18 17:18:32 +0530

    First Commit to Clone branch

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -19,12 +19,27 @@ public class HelloWorld {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 	    
-	    String inputStream = null;
 	    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-	    String name = reader.readLine();
-	    //inputStream = System.console().readLine();
-		System.out.println("Hello " + name);	
+	    int calcOption = getCalcOption(reader);
+	    
+		System.out.print(calcOption);
 
+	}
+
+	/**
+	 * @param reader
+	 * @throws IOException
+	 */
+	private static int getCalcOption(BufferedReader reader) throws IOException {
+		String inputStream;
+		System.out.println("Hello ");
+		System.out.println("Choose one of the options:\n");
+		System.out.println("1.Add:\n");
+		System.out.println("2.Subtract\n");
+		System.out.println("3.Division\n");
+		System.out.println("4.Multiplication:\n");
+		inputStream = reader.readLine();
+		return Integer.parseInt(inputStream);
 	}
 
 }
