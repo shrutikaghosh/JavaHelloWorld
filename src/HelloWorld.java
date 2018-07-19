e73721a9b9e91c3dc35a2e44a4f45a130c57b75d
SGhosh <shrutikaghosh@gmail.com>
2018-07-19 20:05:54 +0530

    Commit to accept interssts

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -17,16 +17,18 @@ public class HelloWorld {
 	 * @throws IOException 
 	 */
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-	    
 	    String inputStream = null;
 	    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 	    System.out.println("Hello, what is your name");	
-	    String name = reader.readLine();
+	    String name=null;
+		name = reader.readLine();
+		
 	    //inputStream = System.console().readLine();
 		System.out.println("Hello " + name + "What is your age?");	
 		inputStream =reader.readLine();
-
+		System.out.println("What are your interests?");	
+		String interest = inputStream =reader.readLine();
+		System.out.println(interest);	
 	}
 
 }
---
+++ src/IOCustomException.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+
+/**
+ * 
+ */
+
+/**
+ * @author SGhosh
+ *
+ */
+public class IOCustomException extends IOException {
+
+	public IOCustomException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public IOCustomException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public IOCustomException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public IOCustomException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
