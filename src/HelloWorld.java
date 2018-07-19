87dae2f7c596b35b5159a672253a75e993b57282
SGhosh <shrutikaghosh@gmail.com>
2018-07-19 17:56:29 +0530

    Initial commit for clone2

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -19,14 +19,22 @@ public class HelloWorld {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 	    
-	    String inputStream = null;
+	    
 	    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 	    System.out.println("Hello, what is your name");	
 	    String name = reader.readLine();
 	    //inputStream = System.console().readLine();
+		int age = getAge(reader, name);
+		
+		System.out.println("Hello" + name + " ,Your age is " + age);
+
+	}
+
+	private static int getAge(BufferedReader reader, String name) throws IOException {
+		String inputStream;
 		System.out.println("Hello " + name + "What is your age?");	
 		inputStream =reader.readLine();
-
+		return Integer.parseInt(inputStream); 
 	}
 
 }
